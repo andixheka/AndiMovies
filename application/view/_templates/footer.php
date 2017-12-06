51cffab5a4a9c1e7e889ea7d1201869838e57a7e
Adalen Vladi <adalenvladi@gmail.com>
2017-12-06 18:15:49 +0100

    framework files +  design

---
+++ application/view/_templates/footer.php
@@ -0,0 +1,123 @@
+      <!-- Page Footer-->
+      <!-- Footer Default-->
+      <footer class="section page-footer page-footer-default text-left bg-gray-darker">
+        <div class="shell-wide">
+          <div class="range range-50 range-xs-center">
+            <div class="cell-sm-6 cell-lg-3">
+              <div class="inset-xl">
+                <h6>About us</h6>
+                <p class="text-spacing-sm">Brave is an innovative online cinema working for you 24/7. We have an extensive collection of movies and TV series, so you just have to select and enjoy what you like most in the world of movies, be it something classic or modern.</p>
+              </div>
+            </div>
+            <div class="cell-sm-6 cell-lg-2">
+              <h6>Quick links</h6>
+              <ul class="list-marked list-marked-primary"> 
+                <li><a href="about-us.html">About</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="shop-4-columns-layout.html">Shop</a></li>
+                <li><a href="classic-blog.html">Blog</a></li>
+                <li><a href="grid-gallery-outside-title.html">Portfolio</a></li>
+                <li><a href="contacts.html">Contacts</a></li>
+              </ul>
+            </div>
+            <div class="cell-sm-6 cell-lg-4">
+              <h6>Instagram feed</h6>
+              <div class="instafeed text-center" data-instafeed-tagname="bMovieTM" data-instafeed-get="tagged" data-instafeed-sort="least-liked" data-lightgallery="group">
+                <div class="range range-10 range-narrow">
+                  <div class="cell-xxs-6 cell-xs-4 cell-sm-4 cell-md-3" data-instafeed-item="">
+                    <div class="thumbnail-instafeed thumbnail-instafeed-minimal"> <a class="instagram-link" data-lightgallery="group-item" href="#" data-images-standard_resolution-url="href"><img class="instagram-image" src="images/_blank.png" alt="" data-images-standard_resolution-url="src"/></a>
+                      <div class="caption"> 
+                        <ul class="list-inline">
+                          <li><span class="icon mdi mdi-heart"></span><span data-likes-count="text"></span></li>
+                          <li><span class="icon mdi mdi-comment"></span><span data-comments-count="text"></span></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="cell-xxs-6 cell-xs-4 cell-sm-4 cell-md-3" data-instafeed-item="">
+                    <div class="thumbnail-instafeed thumbnail-instafeed-minimal"> <a class="instagram-link" data-lightgallery="group-item" href="#" data-images-standard_resolution-url="href"><img class="instagram-image" src="images/_blank.png" alt="" data-images-standard_resolution-url="src"/></a>
+                      <div class="caption"> 
+                        <ul class="list-inline">
+                          <li><span class="icon mdi mdi-heart"></span><span data-likes-count="text"></span></li>
+                          <li><span class="icon mdi mdi-comment"></span><span data-comments-count="text"></span></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="cell-xxs-6 cell-xs-4 cell-sm-4 cell-md-3" data-instafeed-item="">
+                    <div class="thumbnail-instafeed thumbnail-instafeed-minimal"> <a class="instagram-link" data-lightgallery="group-item" href="#" data-images-standard_resolution-url="href"><img class="instagram-image" src="images/_blank.png" alt="" data-images-standard_resolution-url="src"/></a>
+                      <div class="caption"> 
+                        <ul class="list-inline">
+                          <li><span class="icon mdi mdi-heart"></span><span data-likes-count="text"></span></li>
+                          <li><span class="icon mdi mdi-comment"></span><span data-comments-count="text"></span></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="cell-xxs-6 cell-xs-4 cell-sm-4 cell-md-3" data-instafeed-item="">
+                    <div class="thumbnail-instafeed thumbnail-instafeed-minimal"> <a class="instagram-link" data-lightgallery="group-item" href="#" data-images-standard_resolution-url="href"><img class="instagram-image" src="images/_blank.png" alt="" data-images-standard_resolution-url="src"/></a>
+                      <div class="caption"> 
+                        <ul class="list-inline">
+                          <li><span class="icon mdi mdi-heart"></span><span data-likes-count="text"></span></li>
+                          <li><span class="icon mdi mdi-comment"></span><span data-comments-count="text"></span></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="cell-xxs-6 cell-xs-4 cell-sm-4 cell-md-3" data-instafeed-item="">
+                    <div class="thumbnail-instafeed thumbnail-instafeed-minimal"> <a class="instagram-link" data-lightgallery="group-item" href="#" data-images-standard_resolution-url="href"><img class="instagram-image" src="images/_blank.png" alt="" data-images-standard_resolution-url="src"/></a>
+                      <div class="caption"> 
+                        <ul class="list-inline">
+                          <li><span class="icon mdi mdi-heart"></span><span data-likes-count="text"></span></li>
+                          <li><span class="icon mdi mdi-comment"></span><span data-comments-count="text"></span></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="cell-xxs-6 cell-xs-4 cell-sm-4 cell-md-3" data-instafeed-item="">
+                    <div class="thumbnail-instafeed thumbnail-instafeed-minimal"> <a class="instagram-link" data-lightgallery="group-item" href="#" data-images-standard_resolution-url="href"><img class="instagram-image" src="images/_blank.png" alt="" data-images-standard_resolution-url="src"/></a>
+                      <div class="caption"> 
+                        <ul class="list-inline">
+                          <li><span class="icon mdi mdi-heart"></span><span data-likes-count="text"></span></li>
+                          <li><span class="icon mdi mdi-comment"></span><span data-comments-count="text"></span></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="cell-xxs-6 cell-xs-4 cell-sm-4 cell-md-3" data-instafeed-item="">
+                    <div class="thumbnail-instafeed thumbnail-instafeed-minimal"> <a class="instagram-link" data-lightgallery="group-item" href="#" data-images-standard_resolution-url="href"><img class="instagram-image" src="images/_blank.png" alt="" data-images-standard_resolution-url="src"/></a>
+                      <div class="caption"> 
+                        <ul class="list-inline">
+                          <li><span class="icon mdi mdi-heart"></span><span data-likes-count="text"></span></li>
+                          <li><span class="icon mdi mdi-comment"></span><span data-comments-count="text"></span></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="cell-xxs-6 cell-xs-4 cell-sm-4 cell-md-3" data-instafeed-item="">
+                    <div class="thumbnail-instafeed thumbnail-instafeed-minimal"> <a class="instagram-link" data-lightgallery="group-item" href="#" data-images-standard_resolution-url="href"><img class="instagram-image" src="images/_blank.png" alt="" data-images-standard_resolution-url="src"/></a>
+                      <div class="caption"> 
+                        <ul class="list-inline">
+                          <li><span class="icon mdi mdi-heart"></span><span data-likes-count="text"></span></li>
+                          <li><span class="icon mdi mdi-comment"></span><span data-comments-count="text"></span></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <p class="right">&#169;&nbsp;<span class="copyright-year"></span> All Rights Reserved&nbsp;<a href="#">Terms of Use</a>&nbsp;and&nbsp;<a href="privacy-policy.html">Privacy Policy</a></p>
+          <!-- {%FOOTER_LINK}-->
+        </div>
+      </footer>
+    </div>
+
+    <!-- Global Mailform Output-->
+    <div class="snackbars" id="form-output-global"></div>
+    <!-- Javascript-->
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
+  </body><!-- Google Tag Manager --><noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-P9FT69"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P9FT69');</script><!-- End Google Tag Manager -->
+</html>
---
+++ application/view/home/index.php
@@ -0,0 +1,121 @@
+
+
+
+      <!-- Button isotope-->
+      <section class="section section-variant-2 bg-white text-center" style="padding: 0px !important">
+        <div class="shell shell-wide">
+          <div class="isotope-wrap range range-0 range-md-30">
+
+            <div class="cell-lg-12">
+              <div class="isotope isotope-md row" data-isotope-layout="fitRows" data-isotope-group="movies" data-lightgallery="group">
+                <div class="row">
+                  <div class="col-xs-6 col-sm-4 col-md-3 col-xl-2 isotope-item" data-filter="type 3">
+                    <!-- Thumbnail simple-->
+                    <div class="thumbnail-simple"><a class="thumbnail-simple-image-wrap" href="#"><img class="thumbnail-simple-image" src="images/landing-movie-1-270x400.jpg" alt="" width="270" height="400"/></a>
+                      <p class="thumbnail-simple-title"><a href="#">Home Run</a></p>
+                      <p class="thumbnail-simple-subtitle">2013</p>
+                    </div>
+                  </div>
+                  <div class="col-xs-6 col-sm-4 col-md-3 col-xl-2 isotope-item" data-filter="type 2">
+                    <!-- Thumbnail simple-->
+                    <div class="thumbnail-simple"><a class="thumbnail-simple-image-wrap" href="#"><img class="thumbnail-simple-image" src="images/landing-movie-2-270x400.jpg" alt="" width="270" height="400"/></a>
+                      <p class="thumbnail-simple-title"><a href="#">Safe Haven</a></p>
+                      <p class="thumbnail-simple-subtitle">2013</p>
+                    </div>
+                  </div>
+                  <div class="col-xs-6 col-sm-4 col-md-3 col-xl-2 isotope-item" data-filter="type 1">
+                    <!-- Thumbnail simple-->
+                    <div class="thumbnail-simple"><a class="thumbnail-simple-image-wrap" href="#"><img class="thumbnail-simple-image" src="images/landing-movie-3-270x400.jpg" alt="" width="270" height="400"/></a>
+                      <p class="thumbnail-simple-title"><a href="#">Olympus Has Fallen</a></p>
+                      <p class="thumbnail-simple-subtitle">2013</p>
+                    </div>
+                  </div>
+                  <div class="col-xs-6 col-sm-4 col-md-3 col-xl-2 isotope-item" data-filter="type 3">
+                    <!-- Thumbnail simple-->
+                    <div class="thumbnail-simple"><a class="thumbnail-simple-image-wrap" href="#"><img class="thumbnail-simple-image" src="images/landing-movie-4-270x400.jpg" alt="" width="270" height="400"/></a>
+                      <p class="thumbnail-simple-title"><a href="#">Star Trek: Into Darkness</a></p>
+                      <p class="thumbnail-simple-subtitle">2013</p>
+                    </div>
+                  </div>
+                  <div class="col-xs-6 col-sm-4 col-md-3 col-xl-2 isotope-item" data-filter="type 2">
+                    <!-- Thumbnail simple-->
+                    <div class="thumbnail-simple"><a class="thumbnail-simple-image-wrap" href="#"><img class="thumbnail-simple-image" src="images/landing-movie-5-270x400.jpg" alt="" width="270" height="400"/></a>
+                      <p class="thumbnail-simple-title"><a href="#">The Dark Knight</a></p>
+                      <p class="thumbnail-simple-subtitle">2008</p>
+                    </div>
+                  </div>
+                  <div class="col-xs-6 col-sm-4 col-md-3 col-xl-2 isotope-item" data-filter="type 1">
+                    <!-- Thumbnail simple-->
+                    <div class="thumbnail-simple"><a class="thumbnail-simple-image-wrap" href="#"><img class="thumbnail-simple-image" src="images/landing-movie-6-270x400.jpg" alt="" width="270" height="400"/></a>
+                      <p class="thumbnail-simple-title"><a href="#">World War Z</a></p>
+                      <p class="thumbnail-simple-subtitle">2013</p>
+                    </div>
+                  </div>
+                  <div class="col-xs-6 col-sm-4 col-md-3 col-xl-2 isotope-item" data-filter="type 3">
+                    <!-- Thumbnail simple-->
+                    <div class="thumbnail-simple"><a class="thumbnail-simple-image-wrap" href="#"><img class="thumbnail-simple-image" src="images/landing-movie-7-270x400.jpg" alt="" width="270" height="400"/></a>
+                      <p class="thumbnail-simple-title"><a href="#">Shadow</a></p>
+                      <p class="thumbnail-simple-subtitle">2013</p>
+                    </div>
+                  </div>
+                  <div class="col-xs-6 col-sm-4 col-md-3 col-xl-2 isotope-item" data-filter="type 2">
+                    <!-- Thumbnail simple-->
+                    <div class="thumbnail-simple"><a class="thumbnail-simple-image-wrap" href="#"><img class="thumbnail-simple-image" src="images/landing-movie-8-270x400.jpg" alt="" width="270" height="400"/></a>
+                      <p class="thumbnail-simple-title"><a href="#">Silver Linings Playbook</a></p>
+                      <p class="thumbnail-simple-subtitle">2012s</p>
+                    </div>
+                  </div>
+                  <div class="col-xs-6 col-sm-4 col-md-3 col-xl-2 isotope-item" data-filter="type 1">
+                    <!-- Thumbnail simple-->
+                    <div class="thumbnail-simple"><a class="thumbnail-simple-image-wrap" href="#"><img class="thumbnail-simple-image" src="images/landing-movie-9-270x400.jpg" alt="" width="270" height="400"/></a>
+                      <p class="thumbnail-simple-title"><a href="#">Les Misérables</a></p>
+                      <p class="thumbnail-simple-subtitle">2012</p>
+                    </div>
+                  </div>
+                  <div class="col-xs-6 col-sm-4 col-md-3 col-xl-2 isotope-item" data-filter="type 3">
+                    <!-- Thumbnail simple-->
+                    <div class="thumbnail-simple"><a class="thumbnail-simple-image-wrap" href="#"><img class="thumbnail-simple-image" src="images/landing-movie-10-270x400.jpg" alt="" width="270" height="400"/></a>
+                      <p class="thumbnail-simple-title"><a href="#">The Wicked</a></p>
+                      <p class="thumbnail-simple-subtitle">2013</p>
+                    </div>
+                  </div>
+                  <div class="col-xs-6 col-sm-4 col-md-3 col-xl-2 isotope-item" data-filter="type 2">
+                    <!-- Thumbnail simple-->
+                    <div class="thumbnail-simple"><a class="thumbnail-simple-image-wrap" href="#"><img class="thumbnail-simple-image" src="images/landing-movie-11-270x400.jpg" alt="" width="270" height="400"/></a>
+                      <p class="thumbnail-simple-title"><a href="#">The Numbers Station</a></p>
+                      <p class="thumbnail-simple-subtitle">2013</p>
+                    </div>
+                  </div>
+                  <div class="col-xs-6 col-sm-4 col-md-3 col-xl-2 isotope-item" data-filter="type 1">
+                    <!-- Thumbnail simple-->
+                    <div class="thumbnail-simple"><a class="thumbnail-simple-image-wrap" href="#"><img class="thumbnail-simple-image" src="images/landing-movie-12-270x400.jpg" alt="" width="270" height="400"/></a>
+                      <p class="thumbnail-simple-title"><a href="#">The Grandmaster</a></p>
+                      <p class="thumbnail-simple-subtitle">2013</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div><a class="button button-secondary button-nina" href="#">view more movies</a>
+        </div>
+      </section>
+      <!-- CTA-->
+      <section class="section section-xs text-center ">
+        <div class="shell-wide">
+          <div class="box-cta-thin">
+            <p class="big"></p>
+          </div>
+        </div>
+      </section>
+
+
+
+      <!-- CTA-->
+      <section class="section section-xs text-center bg-gradient-1">
+        <div class="shell-wide">
+          <div class="box-cta-thin">
+            <p class="big"><span class="label-cta label-cta-primary">Hot!</span><strong>Brave Theme</strong>&nbsp;<span>has what you need any movie online.&nbsp;</span><a class="link-bold" href="#">Choose it now!  </a></p>
+          </div>
+        </div>
+      </section>
+
