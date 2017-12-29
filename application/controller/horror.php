d5760dcdd4787ecaf200670641ac52b27f30da80
Adalen Vladi <adalenvladi@gmail.com>
2017-12-29 10:12:14 +0100

    example data for testing

---
+++ application/controller/horror.php
@@ -0,0 +1,31 @@
+<?php
+class Horror extends Controller
+{
+    public function index(){
+    	$nextPage='page/1';
+        $query = $this->db->prepare("SELECT * FROM movies WHERE category='horror' order by id desc limit 12");
+        $query->execute();
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/_category/header.php';
+        while ($row=$query->fetch(PDO::FETCH_ASSOC)) { 
+        	require APP . 'view/_category/body.php';
+        }
+        require APP . 'view/_category/footer.php';
+        require APP . 'view/_templates/footer.php';
+    }
+    
+    public function page($page=0){
+        $pager=12*$page;
+        $nextPage=$page+1;
+        $query = $this->db->prepare("SELECT * FROM movies  where  category='horror' order by id desc limit $pager , 12");
+        $query->execute();
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/_category/header.php';
+        while ($row=$query->fetch(PDO::FETCH_ASSOC)) { 
+        	require APP . 'view/_category/body.php';
+        }
+        require APP . 'view/_category/footer.php';
+        require APP . 'view/_templates/footer.php';
+    }
+}
+?>
---
+++ application/view/_category/header.php
@@ -0,0 +1,8 @@
+            <!-- Button isotope-->
+      <section class="section section-variant-2 bg-white text-center" style="padding: 0px !important">
+        <div class="shell shell-wide">
+          <div class="isotope-wrap range range-0 range-md-30">
+
+            <div class="cell-lg-12">
+              <div class="isotope isotope-md row" data-isotope-layout="fitRows" data-isotope-group="movies" data-lightgallery="group">
+                <div class="row">
